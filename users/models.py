78f2160f0eb0f336a20131229c88133a54428647
onyx <onyx@cluster.inmegen.gob.mx>
2019-04-15 13:51:10 -0500

    #3 Permitir a los estudiantes pertenecer a varios grupos de investigación.

--- users/models.py
+++ users/models.py
@@ -3,8 +3,8 @@ from django.db import models
 class User(models.Model):
     uid = models.Autofield(primary_key = True)
     name = models.CharField(250, verbose_name = "Nombre completo")
-    email = models.CharField(320, verbose_name = "Correo electrónico")
-    group = models.ForeignKey('Group', verbose_name = "Grupo de investigación")
+    email = models.EmailField(320, verbose_name = "Correo electrónico")
+    group = models.ManyToManyField('Group', verbose_name = "Grupo de investigación")
     phone = models.CharField(30, verbose_name = "Extensión o teléfono")
     is_employee = models.Boolean()
     expire_date = models.DateField(verbose_name = "¿Cuándo termina el proyecto/postgrado?")
